155f07e1516bf9a19384a7fc8ad68a8e91b60885
Wonicon <conciselove@outlook.com>
2016-04-06 15:37:13 +0800

    Update user interface

--- client/src/scene/hall.c
+++ client/src/scene/hall.c
@@ -14,11 +14,23 @@ struct {
     char *buf;
     size_t len;
     pthread_mutex_t mutex;
+    int cursor_x;
+    int cursor_y;
 } info_bar = {
     NULL,
     0,
-    PTHREAD_MUTEX_INITIALIZER
+    PTHREAD_MUTEX_INITIALIZER,
+    -1
 };
+
+typedef struct {
+    char id[10];
+    uint8_t hp;
+    uint8_t attack;
+} Entity;
+
+Entity rival = { };
+Entity me = { };
 
 /**
  * @brief 获取服务器推送的信息，进行一部分状态转移
@@ -36,7 +48,7 @@ void *push_service(void *arg)
         // 检查是否是无阻塞的更新列表行为
         if (msg.type == LOGIN_ANNOUNCE) {
             pthread_mutex_lock(&mutex_list);
-            player_list = realloc(player_list, (size_t)(nr_players + 1));
+            player_list = realloc(player_list, (size_t)(nr_players + sizeof(PlayerEntry)));
             strcpy(player_list[nr_players].userID, msg.account.id);
             nr_players++;
             pthread_mutex_unlock(&mutex_list);
@@ -61,6 +73,18 @@ void *push_service(void *arg)
         }
 
         switch (client_state) {
+        // 在空闲状态下对远程的接收对战请求进行响应
+        case IDLE:
+            if (msg.type == ASK_BATTLE) {
+                client_state = WAIT_LOCAL_CONFIRM;
+                pthread_mutex_lock(&info_bar.mutex);
+                snprintf(info_bar.buf, info_bar.len, "%s asks for battling (y for yes, n for no):)%n",
+                         msg.account.id, &info_bar.cursor_y);
+                pthread_mutex_unlock(&info_bar.mutex);
+                move(H - 4, info_bar.cursor_y);  // TODO Ugly
+                echo();
+            }
+        // 等待对战请求响应，忽视其他报文，TODO 拒绝新的对战请求
         case WAIT_REMOTE_CONFIRM:
             if (msg.type == NO_BATTLE) {
                 client_state = IDLE;
@@ -88,31 +112,60 @@ void *user_input(void *arg)
         }
 
         switch (client_state) {
-            case IDLE:
-                // 涉及 nr_players 的使用，上锁
-                // TODO 能否认为这样上锁能在数据被更新之前，选定屏幕上确定的玩家？会不会出现以为选了 A 但是实际处理了 B 的情况？
-                pthread_mutex_lock(&mutex_list);
-                if (cmd == KEY_UP && selected > 0) selected--;
-                else if (cmd == KEY_DOWN && selected < nr_players - 1) selected++;
-                else if (cmd == '\n') {
-                    send_invitation_msg();
-                    client_state = WAIT_REMOTE_CONFIRM;
-                }
-                pthread_mutex_unlock(&mutex_list);
-                break;
-            case WAIT_LOCAL_CONFIRM: break;
-            case BATTLING:
-                sleep(1);
+        case IDLE:
+            // 涉及 nr_players 的使用，上锁
+            // TODO 能否认为这样上锁能在数据被更新之前，选定屏幕上确定的玩家？会不会出现以为选了 A 但是实际处理了 B 的情况？
+            pthread_mutex_lock(&mutex_list);
+            if (cmd == KEY_UP && selected > 0) selected--;
+            else if (cmd == KEY_DOWN && selected < nr_players - 1) selected++;
+            else if (cmd == '\n' && selected >= 0 && selected < nr_players) {
+                send_invitation_msg();
+                client_state = WAIT_REMOTE_CONFIRM;
+                pthread_mutex_lock(&info_bar.mutex);
+                snprintf(info_bar.buf, info_bar.len, "waiting %s's response", player_list[selected].userID);
+                pthread_mutex_unlock(&info_bar.mutex);
+            }
+            pthread_mutex_unlock(&mutex_list);
+            break;
+        case WAIT_LOCAL_CONFIRM:
+            if (cmd == 'y') {
+                client_state = BATTLING;
+            }
+            else if (cmd == 'n') {
                 client_state = IDLE;
-                break;
-            default: ;
+            }
+            noecho();
+            break;
+        case BATTLING:
+            sleep(1);
+            client_state = IDLE;
+            break;
+        default: ;
         }
     }
     return NULL;
 }
 
 /**
+ * @brief 绘制对战信息
+ */
+static const char *HP_BAR = "##########################################################################";
+void draw_battle_screen(WINDOW *wind)
+{
+    int h, w;
+    getmaxyx(wind, h, w);
+    int len = 10;
+    mvwprintw(wind, 0, 0, "%.*s", len, rival.id);
+    mvwprintw(wind, 0, len, "%*s", rival.hp * (w - len) / 100, HP_BAR);
+
+    mvwprintw(wind, h - 1, w - len, "%-.*s", len, me.id);
+    mvwprintw(wind, h - 1, 0, "%-*.*s", me.hp * (w - len) / 100, w - len, HP_BAR);
+}
+
+/**
  * @brief 更新列表的视图线程
+ *
+ * TODO 有时候登陆会有乱码，应该是线程安全问题
  */
 void *update_screen(void *arg)
 {
@@ -157,21 +210,27 @@ void *update_screen(void *arg)
 
     WINDOW *win_list = newwin(H - 6, W - 2,     1, 1);
     WINDOW *win_info = newwin(    1, W - 2, H - 4, 1);
-    //WINDOW *win_help = newwin(    1, W - 2, H - 2, 1);
+    WINDOW *win_help = newwin(    1, W - 2, H - 2, 1);
+
+    wprintw(win_help, "q - quit");
+    wrefresh(win_help);
 
     // 更新循环
     while (client_state != QUIT) {
         werase(win_list);
-        pthread_mutex_lock(&mutex_list);
-        if (player_list) {
+        if ((client_state == IDLE || client_state == WAIT_LOCAL_CONFIRM || client_state == WAIT_REMOTE_CONFIRM) && player_list) {
+            pthread_mutex_lock(&mutex_list);
             for (int i = 0; i < nr_players; i++) {
                 // TODO 用颜色高亮
                 // TODO 并发粒度太大，选择卡顿
                 mvwprintw(win_list, i, 0, "[%c] %s", (selected == i ? '*' : ' '), player_list[i].userID);
                 // 这货也不是线程安全的！
             }
+            pthread_mutex_unlock(&mutex_list);
         }
-        pthread_mutex_unlock(&mutex_list);
+        else if (client_state == BATTLING) {
+            draw_battle_screen(win_list);
+        }
         wrefresh(win_list);
 
         werase(win_info);
@@ -189,6 +248,9 @@ void scene_hall(void)
     init_pair(1, COLOR_WHITE, COLOR_BLUE);
 
     client_state = IDLE;
+
+    strncpy(me.id, userID, sizeof(me.id) - 1);
+    me.hp = 100;
 
     erase();
 
